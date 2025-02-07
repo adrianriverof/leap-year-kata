005c76d836e44203db39c7bbbc38245eeb98d654
adrianriverof <adrianriverof@gmail.com>
2025-02-07 11:03:44 +0100

    test: not leap if not divisible by 4

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -8,4 +8,13 @@ public class Tests {
 	public void Test1() {
 		Assert.Pass();
 	}
+
+	[Test]
+	public void if_not_divisible_by_4_is_not_leap()
+	{
+		Leapyearcalculator sut = new Leapyearcalculator();
+		
+		Assert.AreEqual(false, sut.is_leap(5));
+	}
+	
 }
