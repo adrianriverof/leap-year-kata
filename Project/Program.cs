657ccd7fd30241f2e83591716ef9ee44a6685aba
adrianriverof <adrianriverof@gmail.com>
2025-02-07 12:04:50 +0100

    refactor: add constructor to year and internal value
    2 commits en uno tomaaaa

--- Project/Program.cs
+++ Project/Program.cs
@@ -7,6 +7,13 @@ public class Year
 		
 	}
 
+	private readonly int _year;
+
+	public Year(int number_to_be_year)
+	{
+		_year = number_to_be_year;
+	}
+	
 	public bool IsLeap(int year)  
 	{
 		if (((year % 100) == 0) && ((year % 400) != 0) )
